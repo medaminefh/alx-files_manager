1dcbc28dd109b0803ab9eac67b182e73847dae9c
Mohamed Amine Fhal <fhal.mohamed.amine@gmail.com>
2024-06-27 12:04:46 +0100

    add the redis class

---
+++ utils/redis.js
@@ -0,0 +1,39 @@
+import redis from "redis"
+import { promisify } from "util"
+
+class RedisClient {
+    constructor() {
+        this.client = redis.createClient()
+        this.connected = false
+        
+        this.client.on("error", function (error) {
+            console.error(error.message || error.toString())
+            this.connected = false
+            })
+
+        this.client.on("connect", function () {
+            this.connected = true
+        })
+
+        this.client.bind = this.client
+    }
+
+    async set(key, value, duration) {
+        await promisify(this.client.SETEX).bind(this.client)(key, duration, value);
+    }
+
+    async get(key) {
+        return promisify(this.client.GET).bind(this.client)(key);
+    }
+
+    async del(key) {
+        return promisify(this.client.DEL).bind(this.client)(key);
+    }
+
+    isAlive() {
+        return this.connected
+    }
+
+}
+
+export default new RedisClient()
